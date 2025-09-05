358d3f633d66929e32bb8713c65fab73060e0804
zeusthemasterofuniverse <zeusthemasterofuniverse@gmail.com>
2025-09-05 18:29:44 +0530

    Removed Force Scheme
    - Removed Force Scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if (app()->environment() !== 'local') {
+        /*if (app()->environment() !== 'local') {
         URL::forceScheme('https');
-        }
+        }*/
 
         $configData = DB::table('site_configs')->select('config_key','config_name','config_value','config_type','config_max_length')->orderBy('config_order')->get()->toArray();
         $configData = array_column($configData,'config_value','config_key');
