0086d6a9a56107c2a21ba1ce47c73f1253f363d2
zeusthemasterofuniverse <zeusthemasterofuniverse@gmail.com>
2025-09-05 20:29:21 +0530

    Removed Headers
    - Removed Headers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,14 +26,6 @@ class AppServiceProvider extends ServiceProvider
         /*if (app()->environment() !== 'local') {
         URL::forceScheme('https');
         }*/
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-
-        // Handle preflight OPTIONS requests gracefully
-        if (request()->getMethod() === 'OPTIONS') {
-            abort(204); // No content
-        }
 
         $configData = DB::table('site_configs')->select('config_key','config_name','config_value','config_type','config_max_length')->orderBy('config_order')->get()->toArray();
         $configData = array_column($configData,'config_value','config_key');
